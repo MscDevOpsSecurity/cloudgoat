574f9acfc9e9e780368cddf8f913c2fb91840496
juanjoSanz <juanjo.sanzmartin@gmail.com>
2021-04-21 15:17:41 +0200

    .

--- scenarios/lambda_privesc/terraform/iam.tf
+++ scenarios/lambda_privesc/terraform/iam.tf
@@ -53,7 +53,8 @@ data "local_file" "pgp_key" {
 
 resource "aws_iam_user_login_profile" "cg-chris" {
   user    = aws_iam_user.cg-chris.name
-  pgp_key = file("public-key.gpg").content_base64
+  pgp_key = data.local_file.pgp_key.content_base64
+  #file("public-key.gpg").content_base64
   #data.local_file.pgp_key.content_base64
   depends_on = [
     null_resource.export_pgp_file,
